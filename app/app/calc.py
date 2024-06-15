75119fbe7e793b42be1e6ccd87048f2ae6fe9979
basselhalabi17 <basselhalabi17@aucegypt.edu>
2024-06-15 15:57:03 +0300

    add wait for db command

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+    calculates the sum of two numbers
+"""
+
+
+def add(x, y):
+    """adds two numbers
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """ subtracts two numbers
+    """
+    return y-x
