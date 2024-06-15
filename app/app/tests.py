fd299677581537215ccdec7e6af6112914707770
basselhalabi17 <basselhalabi17@aucegypt.edu>
2024-06-15 16:19:32 +0300

    add space

--- app/app/tests.py
+++ app/app/tests.py
@@ -18,4 +18,4 @@ class CalcTests(SimpleTestCase):
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
-        self.assertEqual(res, 5)
+        self.assertEqual(res, 5) 
